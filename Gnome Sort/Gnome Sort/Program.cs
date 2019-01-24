5e5ededd164655abca298c0b3033e0820865291a
usaidalam469 <46966766+usaidalam469@users.noreply.github.com>
2019-01-24 10:24:34 -0800

    Add files via upload

---
+++ Gnome Sort/Gnome Sort/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnome_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sort();
+
+        }
+        static void Sort()
+        {
+            //Intializing
+            int length; int[] arr; int op;
+            char a;
+            Console.WriteLine("---------->GNOME SORT<----------");
+            Console.Write("Enter Length: ");
+            length = Convert.ToInt32 (Console.ReadLine());
+             arr = new int[length];
+            Console.WriteLine("Enter Elements:");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(i + 1 + " element -> ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.Write("ORDER: \n1. Ascending\n2. Descending \nEnter choice: ");
+            op = int.Parse(Console.ReadLine());
+            GnomeSort s = new GnomeSort();
+            switch (op)
+            {
+                case 1:
+                    {
+
+                        arr = s.Ascending(arr);
+                        break;
+                    }
+
+                case 2:
+                    {
+                        arr = s.Decending(arr);
+                        break;
+                    }
+                
+            }
+            //Printing sorted array
+            Console.WriteLine("Sorted Array:");
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+           
+            Console.Write("Press Y to sort again and any key to exit.");
+            a=Convert.ToChar( Console.ReadLine());
+            if (a=='y'||a=='Y')
+            {
+                Console.Clear();
+                Sort();
+            }
+           
+        }
+    }
+    class GnomeSort
+    {
+        public int[] Ascending(int[] arr)
+        {
+            int pos = 0;
+
+            while (pos < arr.Length)
+            {
+                //Comparing
+                if (pos == 0 || arr[pos - 1] <= arr[pos])
+                {
+                    pos++;
+                }
+                else
+                {
+                    //Swapping
+                    int temp = arr[pos];
+                    arr[pos] = arr[pos - 1];
+                    arr[pos - 1] = temp;
+                    pos--;
+                }
+            }
+            //returning sorted array
+            return arr;
+        }
+        public int[] Decending(int[] arr)
+        {
+            int pos = 0;
+            while (pos < arr.Length)
+            {
+                //Comparing
+                if (pos == 0 || arr[pos - 1] >= arr[pos])
+                {
+                    pos++;
+                }
+                else
+                {
+                    //Swapping
+                    int temp = arr[pos];
+                    arr[pos] = arr[pos - 1];
+                    arr[pos - 1] = temp;
+                    pos--;
+                }
+            }
+            //returning sorted array
+            return arr;
+        }
+    }
+}
---
+++ Gnome Sort/Readme.txt
@@ -0,0 +1,17 @@
+-------------------->GNOME SORT<--------------------
+It is the sorting algorithm to sort series of numbers.
+
+How to run?
+Enter the series of numbers and select whether to sort this series in ascending or descending order and then you'll get the required result. 
+
+Works for?
+- Works for every positive or negative integer values.
+- Avoid entering characters or decimal values.
+
+Platforms:
+-Visual Studio
+
+Group Members:
+Syed Mohammad Usaid         17B-063-SE 
+Kainat Nazir                17B-006-SE
+Muhammad Immad Uddin        17B-040-SE
